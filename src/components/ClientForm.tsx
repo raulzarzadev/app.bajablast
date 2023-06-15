f8d05580b4fe67c7560e28c2ef508385f6b34216
Raul Zarza <raulzarza.dev@gmail.com>
2023-06-15 14:26:08 -0700

    feat(clientform): adding medicForm section

--- src/components/ClientForm.tsx
+++ src/components/ClientForm.tsx
@@ -1,12 +1,25 @@
-import { Button, TextField } from '@mui/material'
+import {
+  Autocomplete,
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+  TextareaAutosize,
+  Typography
+} from '@mui/material'
 import { DatePicker } from '@mui/x-date-pickers'
 import { useForm, FormProvider, useFormContext } from 'react-hook-form'
 import { MuiTelInput } from 'mui-tel-input'
+import Modal from './Modal'
+import useModal from '@/hooks/useModal'
+import { CheckBox } from '@mui/icons-material'
+const bloodTypes = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
 
 const ClientForm = () => {
   const methods = useForm()
   const onSubmit = (data) => console.log(data)
-
+  const medicModal = useModal()
   return (
     <FormProvider {...methods}>
       <form
@@ -34,6 +47,69 @@ const ClientForm = () => {
             //methods.setValue('birthday', )
           }}
         />
+
+        {/** **************************************** Medic information */}
+        <Button
+          onClick={(e) => {
+            e.preventDefault()
+            medicModal.handleOpen()
+          }}
+        >
+          Formulario medico
+        </Button>
+        <Modal {...medicModal} title="Formulario Medico">
+          <div className="flex flex-col gap-4">
+            <Typography component={'p'} variant="body2">
+              * Rellena cuidadosamente esta información.{' '}
+            </Typography>
+            <Autocomplete
+              onChange={(e) => {
+                methods.setValue('blood', e)
+              }}
+              renderInput={(params) => (
+                <TextField {...params} label="Tipo de sangre" />
+              )}
+              options={bloodTypes}
+              renderOption={(props, option) => {
+                return (
+                  <li {...props} key={option}>
+                    {option}
+                  </li>
+                )
+              }}
+            />
+            <TextField
+              multiline
+              minRows={4}
+              maxRows={8}
+              fullWidth
+              label="Alergias, enfermedades, padecimientos "
+            />
+            <FormControlLabel
+              required
+              control={<Checkbox />}
+              label="Esta información esta actualizada"
+            />
+          </div>
+        </Modal>
+        {/** **************************************** Terms and conditions */}
+        <Button
+          onClick={(e) => {
+            e.preventDefault()
+            medicModal.handleOpen()
+          }}
+        >
+          Terminos y condiciones
+        </Button>
+        <Modal {...medicModal} title="Formulario Medico">
+          <Box className="flex flex-col gap-4">
+            <Typography component={'p'} variant="body2">
+              Lee cuidadosamente los terminos y condiciones, si estas de acuerdo
+              firma y acepta abajo
+            </Typography>
+          </Box>
+        </Modal>
+
         <Button type="submit">Guardar</Button>
       </form>
     </FormProvider>
