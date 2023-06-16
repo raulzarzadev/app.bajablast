e60cca5842c96ef60207a9db7ae2630f67cdc1df
Raul Zarza <raulzarza.dev@gmail.com>
2023-06-16 12:58:49 -0700

    fix(landing page): now you can se a better desing and the logos of the park

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import PrincipalPage from '@/components/PrincipalPage'
+import Image from 'next/image'
 
 export default function Home() {
   return (
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,11 +1,29 @@
+import { Typography } from '@mui/material'
+import Image from 'next/image'
 import Link from 'next/link'
 
 const LandingPage = () => {
   return (
     <div>
-      <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Bienvenidos a Baja Blast</h1>
-        <Link href={'/profile'}>Registrate</Link>
+      <div className="flex min-h-screen flex-col items-center justify-evenly p-2 text-center ">
+        <Typography variant="body1" component={'header'}>
+          Bienvenidos a
+        </Typography>
+        <Typography variant="h2" component={'h1'}>
+          Baja Blast
+        </Typography>
+        <Typography variant="h4" component={'header'}>
+          Ingresa para descubrir más
+        </Typography>
+        <div className="relative   aspect-[25/16] w-full max-w-md ">
+          <Image
+            fill
+            className="object-cover"
+            src={'/logos/bb-adventure-park.png'}
+            alt="bajablast adventure park"
+          />
+          p
+        </div>
         <span></span>
       </div>
     </div>
