0f33d8d441845a6b4659cd4adaa6134c00a5ac37
Raul Zarza <raulzarza.dev@gmail.com>
2023-06-22 17:58:35 -0700

    fix: fix type in withAuth HOC

--- src/HOCs/withAuth.tsx
+++ src/HOCs/withAuth.tsx
@@ -2,15 +2,14 @@ import { UserContext } from '@/context/user'
 import { NextComponentType } from 'next'
 import { useRouter } from 'next/navigation'
 import { useContext } from 'react'
+
 type WithAuthProps = {
   // Definir aquí las props adicionales que el HOC acepta y pasa al componente envuelto
 }
 
 type WithAuthComponentType<P = {}> = NextComponentType<P & WithAuthProps>
 
-export function withAuth<P extends JSX.IntrinsicAttributes>(
-  Component: WithAuthComponentType<P>
-) {
+export function withAuth<P>(Component: WithAuthComponentType<P>) {
   function WrappedComponent(hocProps: P) {
     const router = useRouter()
     const { user } = useContext(UserContext)
@@ -20,6 +19,7 @@ export function withAuth<P extends JSX.IntrinsicAttributes>(
       return <></>
     }
 
+    //@ts-ignore
     return <Component {...hocProps} />
   }
 
