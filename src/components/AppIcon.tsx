0b763c7b56d87313e4d462e8030a7e0d82aee460
Raul Zarza <raulzarza.dev@gmail.com>
2023-06-29 14:24:02 -0700

    refactor(modalpayment): move modalPayment to a single component

--- src/components/AppIcon.tsx
+++ src/components/AppIcon.tsx
@@ -1,10 +1,12 @@
 import AddIcon from '@mui/icons-material/Add'
 import EditIcon from '@mui/icons-material/Edit'
 import CloseIcon from '@mui/icons-material/Close'
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney'
 const icons = {
   add: <AddIcon />,
   edit: <EditIcon />,
-  close: <CloseIcon />
+  close: <CloseIcon />,
+  money: <AttachMoneyIcon />
 } as const
 type IconName = keyof typeof icons
 const AppIcon = ({ icon }: { icon: IconName }) => {
