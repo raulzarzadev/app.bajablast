8e137f5bfa1a8adc3b2d893e56fce53992461c9c
Raul Zarza <raulzarza.dev@gmail.com>
2023-07-13 06:18:08 -0700

    fix(selectactivity form): now you can see the status of the activity when select in new client

--- src/CONST/activityStatus.ts
+++ src/CONST/activityStatus.ts
@@ -1,8 +1,23 @@
-export const ACTIVITY_STATUS = {
-  OPEN: { label: 'Abierto' },
-  CLOSED: { label: 'Cerrado' },
-  UPCOMING: { label: 'Proximamente' },
-  HIDDEN: { label: 'Oculto' }
+// export const ACTIVITY_STATUS = {
+//   OPEN: { label: 'Abierto' },
+//   CLOSED: { label: 'Cerrado' },
+//   UPCOMING: { label: 'Proximamente' },
+//   HIDDEN: { label: 'Oculto' }
+// } as const
+
+// export type ActivityStatus = keyof typeof ACTIVITY_STATUS
+//
+export enum ACTIVITY_STATUSES {
+  OPEN = 'OPEN',
+  CLOSED = 'CLOSED',
+  UPCOMING = 'UPCOMING',
+  HIDDEN = 'HIDDEN'
+}
+export const ACTIVITY_STATUS: Record<ACTIVITY_STATUSES, { label: string }> = {
+  [ACTIVITY_STATUSES.OPEN]: { label: 'Abierto' },
+  [ACTIVITY_STATUSES.CLOSED]: { label: 'Cerrado' },
+  [ACTIVITY_STATUSES.UPCOMING]: { label: 'Prioximamente' },
+  [ACTIVITY_STATUSES.HIDDEN]: { label: 'Oculto' }
 } as const
 
-export type ActivityStatus = keyof typeof ACTIVITY_STATUS
+export type ActivityStatus = keyof typeof ACTIVITY_STATUSES
--- src/components/SelectParkActivity.tsx
+++ src/components/SelectParkActivity.tsx
@@ -5,6 +5,7 @@ import { NewClientContext } from '@/context/new-client'
 import { Friend, NewClient } from '@/types/user'
 import CurrencySpan from './CurrencySpan'
 import { listenActivities } from '@/firebase/activities'
+import { ACTIVITY_STATUS, ACTIVITY_STATUSES } from '@/CONST/activityStatus'
 
 const SelectParkActivity = ({
   clients,
@@ -26,45 +27,44 @@ const SelectParkActivity = ({
   }, [])
 
   const [activities, setActivities] = useState([])
+
   return (
     <Box>
-      <div>
-        <div>
-          {clients?.map((client, i) => (
-            <div key={client?.id || i}>
-              <Typography variant="h6" className="text-center">
-                {client?.name || ''}
-              </Typography>
-              <div className="flex overflow-x-auto ">
-                <SelectActivity
-                  selected={client?.activity?.name}
-                  activities={activities}
-                  onSelectedActivity={(activity) => {
-                    const aux = [...clients]
-                    if (activity === null) {
-                      aux.splice(i, 1, {
-                        ...client,
-                        activity: null
-                      })
-                      setClients?.(aux)
-                    } else {
-                      aux.splice(i, 1, {
-                        ...client,
-                        activity: {
-                          id: activity.id,
-                          name: activity.name,
-                          price: activity.price
-                        }
-                      })
-                      setClients?.(aux)
-                    }
-                  }}
-                />
-              </div>
+      <Box className="flex overflow-x-auto py-2">
+        {clients?.map((client, i) => (
+          <div key={client?.id || i} className={``}>
+            <Typography variant="h6" className="text-center">
+              {client?.name || ''}
+            </Typography>
+            <div className="flex overflow-x-auto ">
+              <SelectActivity
+                selected={client?.activity?.name}
+                activities={activities}
+                onSelectedActivity={(activity) => {
+                  const aux = [...clients]
+                  if (activity === null) {
+                    aux.splice(i, 1, {
+                      ...client,
+                      activity: null
+                    })
+                    setClients?.(aux)
+                  } else {
+                    aux.splice(i, 1, {
+                      ...client,
+                      activity: {
+                        id: activity.id,
+                        name: activity.name,
+                        price: activity.price
+                      }
+                    })
+                    setClients?.(aux)
+                  }
+                }}
+              />
             </div>
-          ))}
-        </div>
-      </div>
+          </div>
+        ))}
+      </Box>
       <div className="w-full flex justify-evenly">
         <Button
           onClick={() => {
@@ -93,7 +93,14 @@ const SelectParkActivity = ({
     </Box>
   )
 }
-
+const hideActivity = (status: ParkActivity['status']) => {
+  const activities: ParkActivity['status'][] = [
+    ACTIVITY_STATUSES.CLOSED,
+    ACTIVITY_STATUSES.HIDDEN,
+    ACTIVITY_STATUSES.UPCOMING
+  ]
+  return activities.includes(status)
+}
 const SelectActivity = ({
   activities = [],
   onSelectedActivity,
@@ -108,9 +115,12 @@ const SelectActivity = ({
       <button
         className={` 
         w-28 aspect-video  m-1 mt-0 rounded-md shadow-md 
-        ${selected === activity.name && 'bg-blue-300 text-white'}`}
+        ${selected === activity.name && 'bg-blue-300 text-white'}
+        ${hideActivity(activity.status) && ' opacity-25 cursor-not-allowed'}
+        `}
         key={activity.name}
         onClick={(e) => {
+          if (hideActivity(activity.status)) return
           e.preventDefault()
           e.stopPropagation()
           if (selected === activity.name) {
@@ -126,6 +136,9 @@ const SelectActivity = ({
           </Typography>
           <Typography variant="caption" component={'p'}>
             <CurrencySpan quantity={activity.price} />
+          </Typography>
+          <Typography variant="caption" component={'p'}>
+            {ACTIVITY_STATUS[activity.status].label}
           </Typography>
         </div>
       </button>
